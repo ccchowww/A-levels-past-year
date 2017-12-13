a80767b7b4631fd80c942a7c98dd2e5169382af3
ccchowww <ccchowww@gmail.com>
2017-12-13 17:29:14 +0800

    Core functionality 100% done

--- src/App.js
+++ src/App.js
@@ -5,44 +5,151 @@ import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
 
-const subjects = [
-  {
-    subjectName: "Afrikaans - Language (AS Level only) (8679)",
-    code: 8679
-  },
-  {
-    subjectName: "Applied Information and Communication Technology (9713)",
-    code: 9713
-  },
-  {
-    subjectName: "Biology (9700)",
-    code: 9700
-  },
-  {
-    subjectName: "Biology (BES) (9184)",
-    code: 9184
-  },
-  {
-    subjectName: "Business (for first examination in 2016) (9609)",
-    code: 9609
-  },
-  {
-    subjectName: "Business Studies  (9707)",
-    code: 9707
-  },
-  {
-    subjectName: "Chemistry (9701)",
-    code: 9701
-  },
-  {
-    subjectName: "Chinese (A Level only) (9715)",
-    code: 9715
-  },
-  {
-    subjectName: "Computer Science (9608)",
-    code: 9608
-  }
-  ];
+const subjects = [{
+  subjectName: "Accounting (9706)",
+  code: 9706
+},
+{
+  subjectName: "Afrikaans - Language (AS Level only) (8679)",
+  code: 8679
+},
+{
+  subjectName: "Applied Information and Communication Technology (9713)",
+  code: 9713
+},
+{
+  subjectName: "Biology (9700)",
+  code: 9700
+},
+{
+  subjectName: "Biology (BES) (9184)",
+  code: 9184
+},
+{
+  subjectName: "Business (for first examination in 2016) (9609)",
+  code: 9609
+},
+{
+  subjectName: "Business Studies  (9707)",
+  code: 9707
+},
+{
+  subjectName: "Chemistry (9701)",
+  code: 9701
+},
+{
+  subjectName: "Chinese (A Level only) (9715)",
+  code: 9715
+},
+{
+  subjectName: "Computer Science (9608)",
+  code: 9608
+},
+{
+  subjectName: "Computing (9691)",
+  code: 9691
+},
+{
+  subjectName: "Design and Technology (9705)",
+  code: 9705
+},
+{
+  subjectName: "Economics (9708)",
+  code: 9708
+},
+{
+  subjectName: "English - Language AS and A Level  (9093)",
+  code: 9093
+},
+{
+  subjectName: "English - Literature (9695)",
+  code: 9695
+},
+{
+  subjectName: "General Paper 8001 (AS Level only) (8001)",
+  code: 8001
+},
+{
+  subjectName: "General Paper 8004 (AS Level only) (8004)",
+  code: 8004
+},
+{
+  subjectName: "Geography (9696)",
+  code: 9696
+},
+{
+  subjectName: "History (9389)",
+  code: 9389
+},
+{
+  subjectName: "Information Technology (9626)",
+  code: 9626
+},
+{
+  subjectName: "Islamic Studies (8053)",
+  code: 8053
+},
+{
+  subjectName: "Islamic Studies (9013)",
+  code: 9013
+},
+{
+  subjectName: "Law (9084)",
+  code: 9084
+},
+{
+  subjectName: "Marine Science (9693)",
+  code: 9693
+},
+{
+  subjectName: "Mathematics (9709)",
+  code: 9709
+},
+{
+  subjectName: "Mathematics (BES) (9280)",
+  code: 9280
+},
+{
+  subjectName: "Mathematics - Further (9231)",
+  code: 9231
+},
+{
+  subjectName: "Physical Education (9396)",
+  code: 9396
+},
+{
+  subjectName: "Physics (9702)",
+  code: 9702
+},
+{
+  subjectName: "Psychology (9698)",
+  code: 9698
+},
+{
+  subjectName: "Sociology (9699)",
+  code: 9699
+},
+{
+  subjectName: "Spanish - Language (AS Level only) (8685)",
+  code: 8685
+},
+{
+  subjectName: "Spanish - Literature (AS Level only) (8673)",
+  code: 8673
+},
+{
+  subjectName: "Thinking Skills (9694)",
+  code: 9694
+},
+{
+  subjectName: "Travel and Tourism (9395)",
+  code: 9395
+},
+{
+  subjectName: "Urdu - Pakistan only (A Level only) (9686)",
+  code: 9686
+}]
+
 
 const sessions = [
   {
@@ -81,7 +188,7 @@ const sessionList = sessions.map((session, index) => {
 
 class App extends Component {
   state = {
-    subjectSelected: "xd",
+    subjectSelected: "",
     sessionSelected: null,
     yearSelected: "",
     value: "",
@@ -123,9 +230,10 @@ class App extends Component {
       </div>
       <div>
         <TextField
+          floatingLabelStyle={{color: "black"}}
           floatingLabelText="Year eg. 02, 17..."
           onInput={(e)=>{ 
-            e.target.value = Math.max(0, parseInt(e.target.value) ).toString().slice(0,2)
+            e.target.value = Math.max(0, parseInt(e.target.value, 10) ).toString().slice(0,2)
         }}
         min={0}
         type="number"
@@ -136,9 +244,10 @@ class App extends Component {
       </div>
       <div>
         <TextField
+          floatingLabelStyle={{color: "black"}}
           floatingLabelText="Variant eg. 22, 51..."
           onInput={(e)=>{ 
-            e.target.value = Math.max(0, parseInt(e.target.value) ).toString().slice(0,2)
+            e.target.value = Math.max(0, parseInt(e.target.value, 10) ).toString().slice(0,2)
         }}
         min={0}
         type="number"
@@ -146,11 +255,62 @@ class App extends Component {
           value={this.state.name}
           onChange={(e, newValue) => this.setState({ name: newValue})}
         />
-        <p>{"https://papers.gceguide.xyz/A%20Levels/" +
+        </div>
+        <div>
+        <a target="_blank" href={"https://papers.gceguide.xyz/A%20Levels/" +
           this.state.subjectSelected.replace(/ /g,"%20") +
           "/" +
-          this.state.subjectSelected.match(/\d{4}/) + "_" + this.state.sessionSelected + this.state.value + "_" + "qp" + "_" + this.state.name + ".pdf"}</p>
-        <p>{this.state.subjectSelected + "_" + this.state.sessionSelected + this.state.value + "_" + "ms" + "_" + this.state.name + ".pdf"}</p>
+          this.state.subjectSelected.match(/\d{4}/) +
+          "_" +
+          this.state.sessionSelected +
+          this.state.value +
+          "_" +
+          "qp" +
+          "_" +
+          this.state.name +
+          ".pdf"}>
+          {"QP: " +
+          "https://papers.gceguide.xyz/A%20Levels/" +
+          this.state.subjectSelected.replace(/ /g,"%20") +
+          "/" +
+          this.state.subjectSelected.match(/\d{4}/) +
+          "_" +
+          this.state.sessionSelected +
+          this.state.value +
+          "_" +
+          "qp" +
+          "_" +
+          this.state.name +
+          ".pdf"}
+        </a>
+        </div>
+        <div>
+        <a target="_blank" href={"https://papers.gceguide.xyz/A%20Levels/" +
+          this.state.subjectSelected.replace(/ /g,"%20") +
+          "/" +
+          this.state.subjectSelected.match(/\d{4}/) +
+          "_" +
+          this.state.sessionSelected +
+          this.state.value +
+          "_" +
+          "ms" +
+          "_" +
+          this.state.name +
+          ".pdf"}>
+          {"MS: " +
+          "https://papers.gceguide.xyz/A%20Levels/" +
+          this.state.subjectSelected.replace(/ /g,"%20") +
+          "/" +
+          this.state.subjectSelected.match(/\d{4}/) +
+          "_" +
+          this.state.sessionSelected +
+          this.state.value +
+          "_" +
+          "ms" +
+          "_" +
+          this.state.name +
+          ".pdf"}
+        </a>
       </div>
       </MuiThemeProvider>
     );
